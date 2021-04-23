8621aeab694b7af2a9a3bb3955867d91b75e81be
pangqr_ubuntu <impqr@qq.com>
2021-04-23 15:00:01 +0800

    精简Quarkus框架启动时注入逻辑

--- src/main/java/per/pqr/startup/Startup.java
+++ src/main/java/per/pqr/startup/Startup.java
@@ -2,10 +2,8 @@ package per.pqr.startup;
 
 import io.quarkus.runtime.StartupEvent;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 
-@ApplicationScoped
 public class Startup {
     public void onStart(@Observes StartupEvent event) {
         System.out.println("------ Startup Event here ------");
